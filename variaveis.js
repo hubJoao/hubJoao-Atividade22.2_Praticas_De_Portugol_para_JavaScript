3567c48cf160f574c0a5ea49b5d4b7570af1926a
JoaoAbreu <antonio_j_pessoa@estudante.sesisenai.org.br>
2025-09-14 20:29:12 -0300

    criação do arquivo variaveis.js

---
+++ variaveis.js
@@ -0,0 +1,13 @@
+// Atividade Prática 1: Primeiros Códigos (30 min)
+// Exercício 1.1: Variáveis e Console
+
+
+// Complete o código:
+
+let meuNome = "_Jony";
+let minhaIdade = 25;
+let minhaCidade = "Blumenau";
+
+// Exiba as informações usando template literals
+console.log(" Atividade Prática 1: Primeiros Códigos \n");
+console.log(`Meu nome é ${meuNome} tenho ${minhaIdade} moro em ${minhaCidade}`);
