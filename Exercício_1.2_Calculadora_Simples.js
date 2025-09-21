ed970636540d1550cfdb95a035f33c6b2574042e
JoaoAbreu <antonio_j_pessoa@estudante.sesisenai.org.br>
2025-09-21 20:37:53 -0300

    substituindo o arquivo calculadora_simples.js

---
+++ Exercício_1.2_Calculadora_Simples.js
@@ -0,0 +1,19 @@
+
+// Atividade Prática 1: Primeiros Códigos (30 min)
+
+// Exercício 1.2: Calculadora Simples
+
+// Crie variáveis para dois números
+let numero1 = 4;
+let numero2 = 5;
+
+// Calcule e exiba as 4 operações básicas
+let soma =numero1+numero2;
+let subtracao =numero1-numero2;
+let multiplicacao =numero1*numero2;
+let divisao =numero1/numero2;
+
+console.log("\n Soma:", soma);
+console.log("Subtração:",subtracao);
+console.log("Multiplicação:",multiplicacao);
+console.log("Divisão:", divisao);
