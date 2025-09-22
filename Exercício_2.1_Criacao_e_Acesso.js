b3da2dce10769afffcccb683a5687667f13e9445
JoaoAbreu <antonio_j_pessoa@estudante.sesisenai.org.br>
2025-09-21 21:12:10 -0300

    substitue o array_criacao.js

---
+++ Exercício_2.1_Criacao_e_Acesso.js
@@ -0,0 +1,18 @@
+// Atividade Prática 2: Manipulação de Arrays (45 min)
+
+ 
+// Exercício 2.1: Criação e Acesso
+ 
+ 
+// Crie um array com 5 frutas
+let frutas = ["laranja","pêra","maçã","abacaxi","melancia"];
+
+// Acesse e exiba o primeiro e último elemento
+console.log("\n Atividade Prática 2: Manipulação de Arrays (45 min)");
+console.log("\n Exercício 2.1: Criação e Acesso ");
+
+console.log("Primeira fruta:", frutas[0]);
+console.log("Última fruta:", frutas[frutas.length - 1]);
+
+// Exiba o tamanho do array
+console.log("Total de frutas:", frutas.length);
